1a194e60dd25cb12ae222c15f508fc5ff5625c9a
Anthony Cortese <anthonykcortese@gmail.com>
2022-04-14 15:55:12 -0600

    maybe

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,20 @@
+// const config = {
+//   preset: "jest-puppeteer",
+//   collectCoverage: true,
+//   collectCoverageFrom: ["src/**/*"],
+//   coverageReporters: ["text", "lcov", "cobertura"],
+//   setupFilesAfterEnv: ["jest-puppeteer-istanbul/lib/setup"],
+//   reporters: ["default", "jest-puppeteer-istanbul/lib/reporter"],
+//   coverageDirectory: "coverage",
+// };
+// module.exports = config;
+
 const config = {
   preset: "jest-puppeteer",
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*"],
-  coverageReporters: ["text", "lcov", "cobertura"],
-  setupFilesAfterEnv: ["jest-puppeteer-istanbul/lib/setup"],
-  reporters: ["default", "jest-puppeteer-istanbul/lib/reporter"],
-  coverageDirectory: "coverage",
+  globalSetup: "./setup.js",
+  globalTeardown: "./teardown.js",
+  testEnvironment: "./puppeteer_environment.js",
+  testTimeout: 120000,
 };
-module.exports = config;
 
-// module.exports = {
-//   globalSetup: "./setup.js",
-//   globalTeardown: "./teardown.js",
-//   testEnvironment: "./puppeteer_environment.js",
-//   testTimeout: 120000,
-// };
+module.exports = config;
