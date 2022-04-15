5d936f531bebb60c21a5e12125d7b3cea1c49f05
Anthony Cortese <anthonykcortese@gmail.com>
2022-04-15 16:20:01 -0600

    try

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,36 +1,10 @@
-// const ci = Boolean(process.env.CI || false);
-
-// const baseOptions = {
-//   server: {
-//     command: "npm run start",
-//     port: 300,
-//     launchTimeout: 180000,
-//   },
-// };
-
-// const ciPipelineOptions = {
-//   launch: {
-//     headless: true,
-//     args: [
-//       "--no-sandbox",
-//       "--disable-setuid-sandbox",
-//       "--disable-accelerated-2d-canvas",
-//       "--disable-gpu",
-//     ],
-//   },
-
-//   server: baseOptions.server,
-// };
-
-// module.exports = ci ? ciPipelineOptions : baseOptions;
-
 module.exports = {
-  browser: "chromium",
-  browserContext: "default",
-  server: {
-    command: "npm start",
-    port: 3000,
-    launchTimeout: 10000,
-    debug: true,
+  launch: {
+    headless: false,
+    defaultViewport: {
+      width: 1200,
+      height: 800,
+    },
+    args: ["--no-sandbox"],
   },
 };
