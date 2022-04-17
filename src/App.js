cf14f412560634e5c58165ff2321c2f9ec253341
Anthony Cortese <anthonykcortese@gmail.com>
2022-04-17 03:15:40 -0600

    fda

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
