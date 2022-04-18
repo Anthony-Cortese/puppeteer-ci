3328dd2cdd7d89e5ea8b1e6cb850719f9d916bb2
Anthony Cortese <anthonykcortese@gmail.com>
2022-04-18 11:19:35 -0600

    new tests

--- src/App.js
+++ src/App.js
@@ -1,86 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Login from "./components/Login";
-
-// function Home() {
-//   return (
-//     <p className="App-welcome-text">
-//       <code>This is the HomePage</code>
-//     </p>
-//   );
-// }
-
-// function About() {
-//   return <p className="App-welcome-text">This is the about page.</p>;
-// }
-
-// function Login() {
-//   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-//   const [error, setError] = useState(false);
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const login = (event) => {
-//     event.preventDefault();
-
-//     if (email === "username@gmail.com" && password === "password") {
-//       setIsUserLoggedIn(true);
-//     } else {
-//       setIsUserLoggedIn(false);
-//       setError(true);
-
-//       setTimeout(() => {
-//         setError(false);
-//       }, 12000);
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//       <div className="form-wrapper">
-//         <p className="App-welcome-text">This is the Login page.</p>
-//         <h1 className="form-header">Login form</h1>
-//         {!isUserLoggedIn && (
-//           <form className="form" onSubmit={login}>
-//             {error && (
-//               <p className="form-error-text">
-//                 Please enter a correct username/password.
-//               </p>
-//             )}
-
-//             <input
-//               type="email"
-//               required
-//               placeholder="Email Address"
-//               className="form-input form-input__email"
-//               onChange={(e) => {
-//                 setEmail(e.target.value);
-//               }}
-//             />
-//             <input
-//               type="password"
-//               required
-//               placeholder="Password"
-//               className="form-input form-input__password"
-//               onChange={(e) => {
-//                 setPassword(e.target.value);
-//               }}
-//             />
-
-//             <button type="submit" className="form-submit-button">
-//               Button
-//             </button>
-//           </form>
-//         )}
-//         {isUserLoggedIn && (
-//           <p className="form-success-message">You are now signed in.</p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
 
 function App() {
   return (
@@ -89,8 +11,6 @@ function App() {
         <header className="App-header">
           <Header />
           <Routes>
-            {/* <Route path="/about" element={<About />} /> */}
-
             <Route path="/" element={<Home />} />
           </Routes>
           <Login />
